c276ddc3b30fcc95418100536580f21f7c46749c
severak <strajt@dalten.cz>
2023-02-19 01:46:09 +0100

    Skeleton of chatbot as get from first answer.

---
+++ chatbot.js
@@ -0,0 +1,24 @@
+const chatlog = document.getElementById('chatlog');
+const userInput = document.getElementById('userinput');
+
+function handleUserInput(event) {
+  if (event.key === 'Enter') {
+    const inputText = userInput.value;
+    const responseText = generateResponse(inputText);
+    displayMessage('user', inputText);
+    displayMessage('bot', responseText);
+    userInput.value = '';
+  }
+}
+
+function generateResponse(inputText) {
+  // Add your chatbot logic here to generate a response based on the user's input
+  return "I'm sorry, I don't understand.";
+}
+
+function displayMessage(sender, message) {
+  const div = document.createElement('div');
+  div.classList.add(sender);
+  div.innerText = message;
+  chatlog.appendChild(div);
+}
