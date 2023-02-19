9e52daf5a4d719ea86e81f2ed956176850a80d6f
severak <strajt@dalten.cz>
2023-02-19 01:54:08 +0100

    Asked them if it can be make more Eliza-like.

--- chatbot.js
+++ chatbot.js
@@ -12,20 +12,29 @@ function handleUserInput(event) {
 }
 
 function generateResponse(inputText) {
-    let responseText;
-    switch (inputText.toLowerCase()) {
-      case 'hello':
-        responseText = 'Hello there!';
-        break;
-      case 'how are you?':
-        responseText = 'I am doing well, thank you!';
-        break;
-      case 'what is your name?':
-        responseText = "My name is Chatbot. What's yours?";
-        break;
-      default:
-        responseText = "I'm sorry, I don't understand.";
+    const lowerCaseInput = inputText.toLowerCase();
+    let responseText = '';
+  
+    // Simple pattern matching to generate responses
+    if (lowerCaseInput.includes('hello') || lowerCaseInput.includes('hi')) {
+      responseText = 'Hello there! How can I assist you today?';
+    } else if (lowerCaseInput.includes('how are you')) {
+      responseText = 'I am just a computer program, so I do not have feelings like humans do.';
+    } else if (lowerCaseInput.includes('what') && lowerCaseInput.includes('name')) {
+      responseText = "My name is Chatbot. What's yours?";
+    } else if (lowerCaseInput.includes('bye')) {
+      responseText = 'Goodbye!';
+    } else {
+      // If no pattern is matched, generate a generic response
+      const genericResponses = [
+        "I'm sorry, I don't understand.",
+        'Can you please rephrase that?',
+        'I am not sure what you are trying to say.',
+      ];
+      const randomIndex = Math.floor(Math.random() * genericResponses.length);
+      responseText = genericResponses[randomIndex];
     }
+  
     return responseText;
   }
 
